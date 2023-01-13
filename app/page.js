bac22d2b507052c35b228fe500aa7b008d727f6c
unknown <kodenow13@gmail.com>
2023-01-14 02:45:11 +0800

    page

--- app/page.js
+++ app/page.js
@@ -14,22 +14,13 @@ const Page = () => (
   <div className="bg-primary-black overflow-hidden">
     <Navbar />
     <Hero />
-    <div className="relative">
-      <About />
-      <div className="gradient-03 z-0" />
-      <Explore />
-    </div>
-    <div className="relative">
-      <GetStarted />
-      <div className="gradient-04 z-0" />
-      <WhatsNew />
-    </div>
+    <About />
+    <Explore />
+    <GetStarted />
+    <WhatsNew />
     <World />
-    <div className="relative">
-      <Insights />
-      <div className="gradient-04 z-0" />
-      <Feedback />
-    </div>
+    <Insights />
+    <Feedback />
     <Footer />
   </div>
 );
