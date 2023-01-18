e0e8de32de9da89c6abead03bf783f6c9afe52fd
unknown <kodenow13@gmail.com>
2023-01-18 23:33:05 +0800

    fix flex

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -9,19 +9,19 @@ const InsightCard = ({ imgUrl, title, subtitle, index }) => (
     variants={fadeIn("up", "spring", index * 0.5, 1)}
     className="flex md:flex-row flex-col gap-4"
   >
-    <div className="flex flex-col sm:flex-row 2xl:max-w-[1280px] mx-auto justify-between">
+    <div className="flex flex-col md:flex-row 2xl:max-w-[1280px] mx-auto justify-between">
       <img
         src={imgUrl}
         alt=""
-        className=" overflow-hidden sm:w-[30%] md:basis-[40%] lg:basis-[40%] rounded-[32px] "
+        className=" overflow-hidden sm:w-[100%] md:basis-[40%] lg:basis-[40%] rounded-[32px] "
       />
-      <div className="sm:w-[60%]  md:basis-[50%] lg:basis-[40%] justify-between items-center">
+      <div className="sm:w-[100%]  md:basis-[40%] lg:basis-[40%] justify-between items-center">
         <h4 className="font-normal text-[26px] text-white">{title}</h4>
         <p className="mt-[16px] font-normal lg:text-[20px] text-[14px] text-secondary-white">
           {subtitle}
         </p>
       </div>
-      <div className="lg:flex sm:hidden lg:block items-center justify-center w-[50px] h-[50px] rounded-full bg-transparent border-[1px] border-white">
+      <div className="md:flex hidden md:block items-center justify-center w-[50px] h-[50px] rounded-full bg-transparent border-[1px] border-white">
         <img
           src="/arrow.svg"
           alt="arrow"
