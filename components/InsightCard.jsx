6a719493cb854ae4db15bc3d8be0ca1326addcc9
unknown <kodenow13@gmail.com>
2023-01-18 23:16:22 +0800

    feedback

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -9,7 +9,7 @@ const InsightCard = ({ imgUrl, title, subtitle, index }) => (
     variants={fadeIn("up", "spring", index * 0.5, 1)}
     className="flex md:flex-row flex-col gap-4"
   >
-    <div className="flex flex-row 2xl:max-w-[1280px] mx-auto justify-between">
+    <div className="flex flex-col sm:flex-row 2xl:max-w-[1280px] mx-auto justify-between">
       <img
         src={imgUrl}
         alt=""
