36f9ec011fbc5f1851e74968615daa2810ef9482
Kris <kodenow13@gmail.com>
2023-03-29 08:38:15 +0800

    credit

---
+++ components/Credit.jsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+
+const Credit = () => {
+  return (
+    <section className="flex flex-col md:flex-row md:justify-center md:items-center bg-[#4e4c4c] p-1 z-10">
+      <div className="flex justify-center items-center">
+        <p className="text-white">Free template by</p>
+        <Image
+          className="ml-2 mr-1"
+          src="/logoSymbol.png"
+          alt="kodenowLogo"
+          width="30"
+          height="30"
+        />
+        <Image
+          className="mr-3"
+          src="/KodeNowLogo.png"
+          alt="kodenowLogo"
+          width="126"
+          height="20"
+        />
+      </div>
+      <div className="flex justify-center items-center">
+        <h1 className="text-white text-[20px] mr-2 hidden md:block">|</h1>
+        <p className="text-white">Get code here</p>
+        <a
+          href="https://github.com/kodenow/metaverse-nextjs-tailwind"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            className="-ml-2"
+            src="/github.png"
+            alt="kodenowLogo"
+            width="135"
+            height="50"
+          />
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Credit;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,46 +4,11 @@ import { motion } from "framer-motion";
 
 import styles from "../styles";
 import { navVariants } from "../utils/motion";
-import Image from "next/image";
+import Credit from "./Credit";
 
 const Navbar = () => (
   <div>
-    <div className="flex flex-col md:flex-row md:justify-center md:items-center bg-[#4e4c4c] p-1 z-10">
-      <div className="flex justify-center items-center">
-        <p className="text-white">Free template by</p>
-        <Image
-          className="ml-2 mr-1"
-          src="/logoSymbol.png"
-          alt="kodenowLogo"
-          width="30"
-          height="30"
-        />
-        <Image
-          className="mr-3"
-          src="/KodeNowLogo.png"
-          alt="kodenowLogo"
-          width="126"
-          height="20"
-        />
-      </div>
-      <div className="flex justify-center items-center">
-        <h1 className="text-white text-[20px] mr-2 hidden md:block">|</h1>
-        <p className="text-white">Get code here</p>
-        <a
-          href="https://github.com/kodenow/metaverse-nextjs-tailwind"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            className="-ml-2"
-            src="/github.png"
-            alt="kodenowLogo"
-            width="135"
-            height="50"
-          />
-        </a>
-      </div>
-    </div>
+    <Credit />
     <motion.nav
       variants={navVariants}
       initial="hidden"
